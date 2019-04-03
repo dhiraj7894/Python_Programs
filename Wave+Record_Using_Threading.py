da37e7619cb30d67301cff013abbb636378404af
Dhirajlal Vishwakarma <47329713+dhiraj7894@users.noreply.github.com>
2019-04-03 13:01:35 +0530

    Update Wave+Record_Using_Threading.py

--- Wave+Record_Using_Threading.py
+++ Wave+Record_Using_Threading.py
@@ -5,14 +5,18 @@ import matplotlib.pyplot as plt #importing matplotlib for ploting graphs
 import pandas as pd #import pandas
 import wave
 import multiprocessing as mltp
-#import threading
+import time
 
-%matplotlib tk
+CHUNK = 500 * 2 
+FORMAT = pyaudio.paInt16
+RATE = 44100
+
+RECORD_SECONDS = 1
+
+
 def task1():
-    CHUNK = 500 * 2 
-    FORMAT = pyaudio.paInt16
+    %matplotlib tk
     CHANNELS = 1
-    RATE = 44100
     p = pyaudio.PyAudio()
     steam = p.open(
         format = FORMAT,
@@ -33,7 +37,7 @@ def task1():
         data_int = np.array(struct.unpack(str(2*CHUNK)+'B',data),dtype = 'b')[::2]+128
         aS = str(data_int)
     
-        file = open("Auidio","a")
+        file = open("Auidio_1","a")
         file.writelines(aS)
     
         line.set_ydata(data_int)
@@ -41,12 +45,9 @@ def task1():
         fig.canvas.flush_events()
         
 def task2():
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 44100
-    CHUNK = 500 * 2
-    RECORD_SECONDS = 15
-    WAVE_OUTPUT_FILENAME = "file.wav"
+    CHANNELS = 2
+    
+    WAVE_OUTPUT_FILENAME = "file_1.wav"
  
     audio = pyaudio.PyAudio()
  
@@ -61,12 +62,13 @@ def task2():
         data = stream.read(CHUNK)
         frames.append(data)
     print("finished recording",)
- 
+    
  
     # stop Recording
     stream.stop_stream()
     stream.close()
     audio.terminate()
+    
  
     waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
     waveFile.setnchannels(CHANNELS)
@@ -80,6 +82,14 @@ if __name__ == "__main__":
     t2 =mltp.Process(target = task2)
     t1.start()
     t2.start()
+    t = RECORD_SECONDS
+    while t >= 0:
+        print(t, end='...')
+        time.sleep(1)
+        t -= 1
+        if t == 0:
+            t1.terminate()
+            return True 
     t1.join()
     t2.join()
-    print("Done")
+print("Done")
